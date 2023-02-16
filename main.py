9a6d5c51d23b8dd6e6766cff7dbe1b568f483311
Cronista <whamack@live.com>
2023-02-15 23:56:51 -0300

    different comic everyday & raspbian dotenv fix
    updated to return a different comic regardless of the day, though, if the current day of the week is a release day, the new comic will be sent through.
    
    &
    
    added explicit path to the .env file so crontab on raspbian would work properly

--- main.py
+++ main.py
@@ -1,40 +1,79 @@
-import requests, bs4, smtplib, os
+import requests, bs4, smtplib, time, os
 from dotenv import load_dotenv
+from pathlib import Path
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
+#explicitly state the path to the .env file to that raspbian's crontab may read it, if the case (raspberry)
+env_path = Path('.') / '/home/pi/share/xkcdTomail/db.env'
+
 #create a database to store environmental variables to secure smtp login credentials in the code
-load_dotenv('db.env')
+load_dotenv(env_path)
 password = os.environ['PASSWORD']; email = os.environ['EMAIL']; email2 = os.environ['EMAIL2']
+
+#stores the UTC day of the week in a variable using 'time' module
+dayoftheweek = time.gmtime().tm_wday
 
 def xkcdScrap(url):
 
-    #downloads the site into memory
-    res = requests.get(url)
+    #check for day of the week: monday, wednesday and friday, get current comic. All other days: get a random comic
+    if dayoftheweek == 0 or 2 or 4:
 
-    #checks for download errors
-    if not res.status_code == requests.codes.ok:
-        print("Download failed\n")
-        input()
-        exit()
+        #downloads the site into memory
+        res = requests.get(url)
 
-    #uses beautifulsoup to read all the html data (content) from the site
-    soup = bs4.BeautifulSoup(res.content, 'html.parser')
-    #gets the webcomic current no.
-    numsource = bs4.BeautifulSoup(res.text, 'html.parser').select('html body div#middleContainer.box a')[1]['href'].strip()
+        #checks for download errors
+        if not res.status_code == requests.codes.ok:
+            print("Could not reach the site...\n")
+            input()
+            exit()
 
-    #from the html data, locate the image tag, css path, from the comic and selects it's first element [0], wich is the image it self
-    img_tag = soup.select('html body div#middleContainer.box div#comic img')[0]
+        #uses beautifulsoup to read all the html data (content) from the site
+        soup = bs4.BeautifulSoup(res.content, 'html.parser')
 
-    #from the list of elements inside the tag, locate the url source of the image
-    img_url = img_tag['src']
-    img_url_full = 'https:' + img_url
+        #gets the webcomic current no.
+        numsource = bs4.BeautifulSoup(res.text, 'html.parser').select('html body div#middleContainer.box a')[1]['href'].strip()
 
-    #saves the image's byte data into memory
-    img_data = requests.get(img_url_full).content
+        #from the html data, locate the image tag, css path, from the new comic and selects it's first element [0], wich is the image it self
+        img_tag = soup.select('html body div#middleContainer.box div#comic img')[0]
 
-    return img_data, numsource
+        #from the list of elements inside the tag, locate the url source of the image
+        img_url = img_tag['src']
+        img_url_full = 'https:' + img_url
+
+        #saves the image's byte data into memory
+        img_data = requests.get(img_url_full).content
+
+        return img_data, numsource
+    else:
+        
+        #downloads the random comic page into memory
+        res = requests.get('https://c.xkcd.com/random/comic/')
+
+        #checks for download errors
+        if not res.status_code == requests.codes.ok:
+            print("Could not reach the site...\n")
+            input()
+            exit()
+
+        #uses beautifulsoup to read all the html data (content) from the site
+        soup = bs4.BeautifulSoup(res.content, 'html.parser')
+
+        #gets the random webcomic current no.
+        numsource = bs4.BeautifulSoup(res.text, 'html.parser').select('html body div#middleContainer.box a')[1]['href'].strip()
+
+        #from the html data, locate the image tag, css path, from the random comic and selects it's first element [0], wich is the image it self
+        img_tag = soup.select('html body div#middleContainer.box div#comic img')[0]
+
+        #from the list of elements inside the tag, locate the url source of the image
+        img_url = img_tag['src']
+        img_url_full = 'https:' + img_url
+
+        #saves the image's byte data into memory
+        img_data = requests.get(img_url_full).content
+
+        return img_data, numsource
 
 def xkcdSend(img, num):
 
